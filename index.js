23701fc1265feffaf6e58c8645d1053161917444
Joey Pham <joeypham@joeys-mbp.mynetworksettings.com>
2023-06-10 15:47:20 -0400

    added main menu function and answers

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 const inquirer = require("inquirer");
+const connection = require("./config/connection");
+
+function displayMainMenu() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "View all departments",
+        "View all roles",
+        "View all employees",
+        "Add a department",
+        "Add a role",
+        "Add an employee",
+        "Update an employee role",
+        "exit",
+      ],
+    })
+    .then((amswers) => {
+      switch (amswers.action) {
+        case "View all departments":
+          viewDepartments();
+          break;
+        case "View all roles":
+          viewRoles();
+          break;
+        case "View all employees":
+          viewEmployees();
+          break;
+        case "Add a department":
+          addDepartment();
+          break;
+        case "Add a role":
+          addRole();
+          break;
+        case "Add a employee":
+          addEmployee();
+          break;
+        case "Update an employee role":
+          updateEmployeeRole();
+          break;
+        case "Exit":
+          connection.end();
+          console.log("Goodbye!");
+          break;
+      }
+    });
+}
